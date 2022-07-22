833e9b8a51036e196bf15e6ff7598fffa3be12ca
30005-李明新 <limingxin2720@163.com>
2022-07-22 23:11:47 +0800

    feat: add code about goroutine,channel,assertion,reflect

---
+++ golearn-basic/chapter13-context/example1/example1.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	flag := make(chan bool)
+	message := make(chan int)
+	go son(flag, message)
+	for i := 0; i < 10; i++ {
+		message <- i
+	}
+	flag <- true
+	time.Sleep(time.Second)
+}
+
+func son(flag chan bool, msg chan int) {
+	t := time.Tick(time.Second)
+	for _ = range t {
+		select {
+		case m := <-msg:
+			fmt.Printf("接收到值%d\n", m)
+		case <-flag:
+			fmt.Println("我结束了")
+			return
+		}
+	}
+}
